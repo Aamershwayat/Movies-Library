73bf308c268aa6504372010514f200ac6eb98dab
Aamershwayat <amerabdalhameed77@gmail.com>
2022-02-10 00:44:49 +0200

    task 12

--- test.js
+++ test.js
@@ -1,33 +1,33 @@
-const muli = require('serve.js')
+// const muli = require('serve.js')
 
-const app = express();
-app.listen (
+// const app = express();
+// app.listen (
     
-)
+// )
 
 
 
 
 
-function muli(x,y){
-    return x * y ;
-}
+// function muli(x,y){
+//     return x * y ;
+// }
 
 
-function Employee(name, id , dep){
-    this.name = name ;
-    this.id = id;
-    this.dep = dep;
-    }
+// function Employee(name, id , dep){
+//     this.name = name ;
+//     this.id = id;
+//     this.dep = dep;
+//     }
     
-    Employee.prototype.console = function(){
+//     Employee.prototype.console = function(){
     
-        console.log("hello")
-    }
+//         console.log("hello")
+//     }
     
-     const ahmad = new Employee("Ahmad","2139e","development")
-     ahmad.console(); 
-    module.exports = muli;
+//      const ahmad = new Employee("Ahmad","2139e","development")
+//      ahmad.console(); 
+//     module.exports = muli;
 
 
 
